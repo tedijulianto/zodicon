2cfafbb565878441ee5695c14c093b66a5adc782
tedijulianto <tedijulianto92@gmail.com>
2022-06-22 16:19:47 +0700

    deactivate vanila-tilt.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
   </section>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="{{ asset('js/vanilla-tilt.js') }}"></script>
+  {{-- <script src="{{ asset('js/vanilla-tilt.js') }}"></script> --}}
   <script src="{{ asset('js/script.js') }}"></script>
 
 </body>
