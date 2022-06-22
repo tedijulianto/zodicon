0702fc70eaf782c9a4f132bd28f8cd49b10507bc
tedijulianto <tedijulianto92@gmail.com>
2022-06-22 13:49:25 +0700

    Finalized Coming Soon page

---
+++ resources/views/coming_soon.blade.php
@@ -0,0 +1,225 @@
+<div class="bg-hero">
+  <img src="{{ asset('images/title.png') }}" alt="Releasing Soon" class="imgTitle">
+  <div class="sosmed">
+    <a href="https://www.instagram.com/zodicon.nft/"
+      class="d-flex justify-content-center align-items-center sosmedIcon">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path fill="white" fill-rule="evenodd"
+          d="M7.465 1.066C8.638 1.012 9.012 1 12 1c2.988 0 3.362.013 4.534.066c1.172.053 1.972.24 2.672.511c.733.277 1.398.71 1.948 1.27c.56.549.992 1.213 1.268 1.947c.272.7.458 1.5.512 2.67C22.988 8.639 23 9.013 23 12c0 2.988-.013 3.362-.066 4.535c-.053 1.17-.24 1.97-.512 2.67a5.396 5.396 0 0 1-1.268 1.949c-.55.56-1.215.992-1.948 1.268c-.7.272-1.5.458-2.67.512c-1.174.054-1.548.066-4.536.066c-2.988 0-3.362-.013-4.535-.066c-1.17-.053-1.97-.24-2.67-.512a5.397 5.397 0 0 1-1.949-1.268a5.392 5.392 0 0 1-1.269-1.948c-.271-.7-.457-1.5-.511-2.67C1.012 15.361 1 14.987 1 12c0-2.988.013-3.362.066-4.534c.053-1.172.24-1.972.511-2.672a5.396 5.396 0 0 1 1.27-1.948a5.392 5.392 0 0 1 1.947-1.269c.7-.271 1.5-.457 2.67-.511Zm8.98 1.98c-1.16-.053-1.508-.064-4.445-.064c-2.937 0-3.285.011-4.445.064c-1.073.049-1.655.228-2.043.379c-.513.2-.88.437-1.265.822a3.412 3.412 0 0 0-.822 1.265c-.151.388-.33.97-.379 2.043c-.053 1.16-.064 1.508-.064 4.445c0 2.937.011 3.285.064 4.445c.049 1.073.228 1.655.379 2.043c.176.477.457.91.822 1.265c.355.365.788.646 1.265.822c.388.151.97.33 2.043.379c1.16.053 1.507.064 4.445.064c2.938 0 3.285-.011 4.445-.064c1.073-.049 1.655-.228 2.043-.379c.513-.2.88-.437 1.265-.822c.365-.355.646-.788.822-1.265c.151-.388.33-.97.379-2.043c.053-1.16.064-1.508.064-4.445c0-2.937-.011-3.285-.064-4.445c-.049-1.073-.228-1.655-.379-2.043c-.2-.513-.437-.88-.822-1.265a3.413 3.413 0 0 0-1.265-.822c-.388-.151-.97-.33-2.043-.379Zm-5.85 12.345a3.669 3.669 0 0 0 4-5.986a3.67 3.67 0 1 0-4 5.986ZM8.002 8.002a5.654 5.654 0 1 1 7.996 7.996a5.654 5.654 0 0 1-7.996-7.996Zm10.906-.814a1.337 1.337 0 1 0-1.89-1.89a1.337 1.337 0 0 0 1.89 1.89Z"
+          clip-rule="evenodd" />
+      </svg>
+    </a>
+    <a href="https://twitter.com/zodicon" class="d-flex justify-content-center align-items-center sosmedIcon">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path fill="white"
+          d="M22.991 3.95a1 1 0 0 0-1.51-.86a7.48 7.48 0 0 1-1.874.794a5.152 5.152 0 0 0-3.374-1.242a5.232 5.232 0 0 0-5.223 5.063a11.032 11.032 0 0 1-6.814-3.924a1.012 1.012 0 0 0-.857-.365a.999.999 0 0 0-.785.5a5.276 5.276 0 0 0-.242 4.769l-.002.001a1.041 1.041 0 0 0-.496.89a3.042 3.042 0 0 0 .027.439a5.185 5.185 0 0 0 1.568 3.312a.998.998 0 0 0-.066.77a5.204 5.204 0 0 0 2.362 2.922a7.465 7.465 0 0 1-3.59.448A1 1 0 0 0 1.45 19.3a12.942 12.942 0 0 0 7.01 2.061a12.788 12.788 0 0 0 12.465-9.363a12.822 12.822 0 0 0 .535-3.646l-.001-.2a5.77 5.77 0 0 0 1.532-4.202Zm-3.306 3.212a.995.995 0 0 0-.234.702c.01.165.009.331.009.488a10.824 10.824 0 0 1-.454 3.08a10.685 10.685 0 0 1-10.546 7.93a10.938 10.938 0 0 1-2.55-.301a9.48 9.48 0 0 0 2.942-1.564a1 1 0 0 0-.602-1.786a3.208 3.208 0 0 1-2.214-.935q.224-.042.445-.105a1 1 0 0 0-.08-1.943a3.198 3.198 0 0 1-2.25-1.726a5.3 5.3 0 0 0 .545.046a1.02 1.02 0 0 0 .984-.696a1 1 0 0 0-.4-1.137a3.196 3.196 0 0 1-1.425-2.673c0-.066.002-.133.006-.198a13.014 13.014 0 0 0 8.21 3.48a1.02 1.02 0 0 0 .817-.36a1 1 0 0 0 .206-.867a3.157 3.157 0 0 1-.087-.729a3.23 3.23 0 0 1 3.226-3.226a3.184 3.184 0 0 1 2.345 1.02a.993.993 0 0 0 .921.298a9.27 9.27 0 0 0 1.212-.322a6.681 6.681 0 0 1-1.026 1.524Z" />
+      </svg>
+    </a>
+    <a href="https://discord.gg/xtVJ7pnyXH" class="d-flex justify-content-center align-items-center sosmedIcon">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <g fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+          <path d="M5.5 16c5 2.5 8 2.5 13 0" />
+          <path d="m15.5 17.5l1 2s4.171-1.328 5.5-3.5c0-1 .53-8.147-3-10.5c-1.5-1-4-1.5-4-1.5l-1 2h-2" />
+          <path d="m8.528 17.5l-1 2s-4.171-1.328-5.5-3.5c0-1-.53-8.147 3-10.5c1.5-1 4-1.5 4-1.5l1 2h2" />
+          <path
+            d="M8.5 14c-.828 0-1.5-.895-1.5-2s.672-2 1.5-2s1.5.895 1.5 2s-.672 2-1.5 2Zm7 0c-.828 0-1.5-.895-1.5-2s.672-2 1.5-2s1.5.895 1.5 2s-.672 2-1.5 2Z" />
+        </g>
+      </svg>
+    </a>
+    <a href="https://medium.com/@zodicon" class="d-flex justify-content-center align-items-center sosmedIcon">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+        <circle cx="13.979" cy="24" r="10.479" fill="none" stroke="white" stroke-linecap="round" stroke-width="3"
+          stroke-linejoin="round" />
+        <ellipse cx="32.318" cy="24" fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round"
+          stroke-width="3" rx="4.934" ry="10.479" />
+        <ellipse cx="42.339" cy="24" fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round"
+          stroke-width="3" rx="2.161" ry="10.479" />
+      </svg>
+    </a>
+  </div>
+
+  {{-- <div class="textTitle d-flex flex-column align-items-center">
+    <div class="title d-flex flex-column align-items-center">
+      <h2 data-coming="Coming" class="coming">Coming</h2>
+      <h2 data-soon="Soon" class="soon">Soon</h2>
+    </div>
+  </div>
+
+  <img src="{{ asset('images/nft.png') }}" alt="showcase" class="imageShowcase">
+  <svg viewBox="0 0 428 320" xmlns="http://www.w3.org/2000/svg" class="light">
+    <path d="M0 320V0H428V320H0Z" fill="url(#paint0_linear_661_8)" />
+    <defs>
+      <linearGradient id="paint0_linear_661_8" x1="214" y1="1.40838e-08" x2="214" y2="385.587"
+        gradientUnits="userSpaceOnUse">
+        <stop stop-color="#0057FF" stop-opacity="0" />
+        <stop offset="1" stop-color="#2D549D" />
+      </linearGradient>
+    </defs>
+  </svg>
+  <svg viewBox="0 0 488 244" xmlns="http://www.w3.org/2000/svg" class="stage">
+    <path
+      d="M488 134.133C488 194.811 378.758 244 244 244C109.243 244 0 194.811 0 134.133C44.5 -12.5398 109.243 24.2656 244 24.2656C378.758 24.2656 440 -17.4839 488 134.133Z"
+      fill="#11367C" />
+    <path
+      d="M477 106.951C477 147.614 372.906 196 244.5 196C116.094 196 12 147.614 12 106.951C38.4873 -4.98134 116.094 33.3245 244.5 33.3245C372.906 33.3245 440.212 -2.49394 477 106.951Z"
+      fill="#113F96" />
+    <g filter="url(#filter0_d_663_2)">
+      <ellipse cx="244" cy="74" rx="220" ry="74" fill="url(#paint0_linear_663_2)" />
+    </g>
+    <defs>
+      <filter id="filter0_d_663_2" x="23" y="0" width="442" height="176" filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha" />
+        <feOffset dy="27" />
+        <feGaussianBlur stdDeviation="0.5" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0.0685416 0 0 0 0 0.247995 0 0 0 0 0.5875 0 0 0 1 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_663_2" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_663_2" result="shape" />
+      </filter>
+      <linearGradient id="paint0_linear_663_2" x1="244" y1="0" x2="244" y2="148" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#105074" />
+        <stop offset="1" stop-color="#2B60E8" />
+      </linearGradient>
+    </defs>
+  </svg>
+
+  <div class="container-fluids">
+    <div class="d-flex justify-content-center align-items-center">
+      <div class="borderGradient">
+        <h6 class="mb-1 fw-bold text-black" style="font-family: 'Roboto', sans-serif;">Follow Us</h6>
+        <div class="cardParent">
+          <div class="cardContent">
+            <div class="cardBg"></div>
+            <a href="https://www.instagram.com/zodicon.nft/">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path fill="black" fill-rule="evenodd"
+                  d="M7.465 1.066C8.638 1.012 9.012 1 12 1c2.988 0 3.362.013 4.534.066c1.172.053 1.972.24 2.672.511c.733.277 1.398.71 1.948 1.27c.56.549.992 1.213 1.268 1.947c.272.7.458 1.5.512 2.67C22.988 8.639 23 9.013 23 12c0 2.988-.013 3.362-.066 4.535c-.053 1.17-.24 1.97-.512 2.67a5.396 5.396 0 0 1-1.268 1.949c-.55.56-1.215.992-1.948 1.268c-.7.272-1.5.458-2.67.512c-1.174.054-1.548.066-4.536.066c-2.988 0-3.362-.013-4.535-.066c-1.17-.053-1.97-.24-2.67-.512a5.397 5.397 0 0 1-1.949-1.268a5.392 5.392 0 0 1-1.269-1.948c-.271-.7-.457-1.5-.511-2.67C1.012 15.361 1 14.987 1 12c0-2.988.013-3.362.066-4.534c.053-1.172.24-1.972.511-2.672a5.396 5.396 0 0 1 1.27-1.948a5.392 5.392 0 0 1 1.947-1.269c.7-.271 1.5-.457 2.67-.511Zm8.98 1.98c-1.16-.053-1.508-.064-4.445-.064c-2.937 0-3.285.011-4.445.064c-1.073.049-1.655.228-2.043.379c-.513.2-.88.437-1.265.822a3.412 3.412 0 0 0-.822 1.265c-.151.388-.33.97-.379 2.043c-.053 1.16-.064 1.508-.064 4.445c0 2.937.011 3.285.064 4.445c.049 1.073.228 1.655.379 2.043c.176.477.457.91.822 1.265c.355.365.788.646 1.265.822c.388.151.97.33 2.043.379c1.16.053 1.507.064 4.445.064c2.938 0 3.285-.011 4.445-.064c1.073-.049 1.655-.228 2.043-.379c.513-.2.88-.437 1.265-.822c.365-.355.646-.788.822-1.265c.151-.388.33-.97.379-2.043c.053-1.16.064-1.508.064-4.445c0-2.937-.011-3.285-.064-4.445c-.049-1.073-.228-1.655-.379-2.043c-.2-.513-.437-.88-.822-1.265a3.413 3.413 0 0 0-1.265-.822c-.388-.151-.97-.33-2.043-.379Zm-5.85 12.345a3.669 3.669 0 0 0 4-5.986a3.67 3.67 0 1 0-4 5.986ZM8.002 8.002a5.654 5.654 0 1 1 7.996 7.996a5.654 5.654 0 0 1-7.996-7.996Zm10.906-.814a1.337 1.337 0 1 0-1.89-1.89a1.337 1.337 0 0 0 1.89 1.89Z"
+                  clip-rule="evenodd" />
+              </svg>
+            </a>
+          </div>
+
+          <div class="cardContent">
+            <div class="cardBg"></div>
+            <a href="https://twitter.com/zodicon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path fill="black"
+                  d="M22.991 3.95a1 1 0 0 0-1.51-.86a7.48 7.48 0 0 1-1.874.794a5.152 5.152 0 0 0-3.374-1.242a5.232 5.232 0 0 0-5.223 5.063a11.032 11.032 0 0 1-6.814-3.924a1.012 1.012 0 0 0-.857-.365a.999.999 0 0 0-.785.5a5.276 5.276 0 0 0-.242 4.769l-.002.001a1.041 1.041 0 0 0-.496.89a3.042 3.042 0 0 0 .027.439a5.185 5.185 0 0 0 1.568 3.312a.998.998 0 0 0-.066.77a5.204 5.204 0 0 0 2.362 2.922a7.465 7.465 0 0 1-3.59.448A1 1 0 0 0 1.45 19.3a12.942 12.942 0 0 0 7.01 2.061a12.788 12.788 0 0 0 12.465-9.363a12.822 12.822 0 0 0 .535-3.646l-.001-.2a5.77 5.77 0 0 0 1.532-4.202Zm-3.306 3.212a.995.995 0 0 0-.234.702c.01.165.009.331.009.488a10.824 10.824 0 0 1-.454 3.08a10.685 10.685 0 0 1-10.546 7.93a10.938 10.938 0 0 1-2.55-.301a9.48 9.48 0 0 0 2.942-1.564a1 1 0 0 0-.602-1.786a3.208 3.208 0 0 1-2.214-.935q.224-.042.445-.105a1 1 0 0 0-.08-1.943a3.198 3.198 0 0 1-2.25-1.726a5.3 5.3 0 0 0 .545.046a1.02 1.02 0 0 0 .984-.696a1 1 0 0 0-.4-1.137a3.196 3.196 0 0 1-1.425-2.673c0-.066.002-.133.006-.198a13.014 13.014 0 0 0 8.21 3.48a1.02 1.02 0 0 0 .817-.36a1 1 0 0 0 .206-.867a3.157 3.157 0 0 1-.087-.729a3.23 3.23 0 0 1 3.226-3.226a3.184 3.184 0 0 1 2.345 1.02a.993.993 0 0 0 .921.298a9.27 9.27 0 0 0 1.212-.322a6.681 6.681 0 0 1-1.026 1.524Z" />
+              </svg>
+            </a>
+          </div>
+
+          <div class="cardContent">
+            <div class="cardBg"></div>
+            <a href="https://discord.gg/aTBCw8hg">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <g fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                  <path d="M5.5 16c5 2.5 8 2.5 13 0" />
+                  <path d="m15.5 17.5l1 2s4.171-1.328 5.5-3.5c0-1 .53-8.147-3-10.5c-1.5-1-4-1.5-4-1.5l-1 2h-2" />
+                  <path d="m8.528 17.5l-1 2s-4.171-1.328-5.5-3.5c0-1-.53-8.147 3-10.5c1.5-1 4-1.5 4-1.5l1 2h2" />
+                  <path
+                    d="M8.5 14c-.828 0-1.5-.895-1.5-2s.672-2 1.5-2s1.5.895 1.5 2s-.672 2-1.5 2Zm7 0c-.828 0-1.5-.895-1.5-2s.672-2 1.5-2s1.5.895 1.5 2s-.672 2-1.5 2Z" />
+                </g>
+              </svg>
+            </a>
+          </div>
+
+          <div class="cardContent">
+            <div class="cardBg"></div>
+            <a href="https://medium.com/@zodicon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+                <circle cx="13.979" cy="24" r="10.479" fill="none" stroke="black" stroke-linecap="round"
+                  stroke-width="3" stroke-linejoin="round" />
+                <ellipse cx="32.318" cy="24" fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round"
+                  stroke-width="3" rx="4.934" ry="10.479" />
+                <ellipse cx="42.339" cy="24" fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round"
+                  stroke-width="3" rx="2.161" ry="10.479" />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="patternTop">
+    <div class="item item1"></div>
+    <div class="item item2"></div>
+    <div class="item item3"></div>
+    <div class="item item4"></div>
+    <div class="item item5"></div>
+    <div class="item item6"></div>
+  </div>
+
+  <div class="patternBottom">
+    <div class="item item1"></div>
+    <div class="item item2"></div>
+    <div class="item item3"></div>
+    <div class="item item4"></div>
+    <div class="item item5"></div>
+    <div class="item item6"></div>
+  </div>
+
+  <div class="container-fluids">
+    <div class="d-flex justify-content-center align-items-center">
+      <div class="borderGradient">
+        <h6 class="mb-1 fw-bold text-black" style="font-family: 'Roboto', sans-serif;">Follow Us</h6>
+        <div class="cardParent">
+          <div class="cardContent">
+            <div class="cardBg"></div>
+            <a href="https://www.instagram.com/zodicon.nft/">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path fill="black" fill-rule="evenodd"
+                  d="M7.465 1.066C8.638 1.012 9.012 1 12 1c2.988 0 3.362.013 4.534.066c1.172.053 1.972.24 2.672.511c.733.277 1.398.71 1.948 1.27c.56.549.992 1.213 1.268 1.947c.272.7.458 1.5.512 2.67C22.988 8.639 23 9.013 23 12c0 2.988-.013 3.362-.066 4.535c-.053 1.17-.24 1.97-.512 2.67a5.396 5.396 0 0 1-1.268 1.949c-.55.56-1.215.992-1.948 1.268c-.7.272-1.5.458-2.67.512c-1.174.054-1.548.066-4.536.066c-2.988 0-3.362-.013-4.535-.066c-1.17-.053-1.97-.24-2.67-.512a5.397 5.397 0 0 1-1.949-1.268a5.392 5.392 0 0 1-1.269-1.948c-.271-.7-.457-1.5-.511-2.67C1.012 15.361 1 14.987 1 12c0-2.988.013-3.362.066-4.534c.053-1.172.24-1.972.511-2.672a5.396 5.396 0 0 1 1.27-1.948a5.392 5.392 0 0 1 1.947-1.269c.7-.271 1.5-.457 2.67-.511Zm8.98 1.98c-1.16-.053-1.508-.064-4.445-.064c-2.937 0-3.285.011-4.445.064c-1.073.049-1.655.228-2.043.379c-.513.2-.88.437-1.265.822a3.412 3.412 0 0 0-.822 1.265c-.151.388-.33.97-.379 2.043c-.053 1.16-.064 1.508-.064 4.445c0 2.937.011 3.285.064 4.445c.049 1.073.228 1.655.379 2.043c.176.477.457.91.822 1.265c.355.365.788.646 1.265.822c.388.151.97.33 2.043.379c1.16.053 1.507.064 4.445.064c2.938 0 3.285-.011 4.445-.064c1.073-.049 1.655-.228 2.043-.379c.513-.2.88-.437 1.265-.822c.365-.355.646-.788.822-1.265c.151-.388.33-.97.379-2.043c.053-1.16.064-1.508.064-4.445c0-2.937-.011-3.285-.064-4.445c-.049-1.073-.228-1.655-.379-2.043c-.2-.513-.437-.88-.822-1.265a3.413 3.413 0 0 0-1.265-.822c-.388-.151-.97-.33-2.043-.379Zm-5.85 12.345a3.669 3.669 0 0 0 4-5.986a3.67 3.67 0 1 0-4 5.986ZM8.002 8.002a5.654 5.654 0 1 1 7.996 7.996a5.654 5.654 0 0 1-7.996-7.996Zm10.906-.814a1.337 1.337 0 1 0-1.89-1.89a1.337 1.337 0 0 0 1.89 1.89Z"
+                  clip-rule="evenodd" />
+              </svg>
+            </a>
+          </div>
+
+          <div class="cardContent">
+            <div class="cardBg"></div>
+            <a href="https://twitter.com/zodicon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path fill="black"
+                  d="M22.991 3.95a1 1 0 0 0-1.51-.86a7.48 7.48 0 0 1-1.874.794a5.152 5.152 0 0 0-3.374-1.242a5.232 5.232 0 0 0-5.223 5.063a11.032 11.032 0 0 1-6.814-3.924a1.012 1.012 0 0 0-.857-.365a.999.999 0 0 0-.785.5a5.276 5.276 0 0 0-.242 4.769l-.002.001a1.041 1.041 0 0 0-.496.89a3.042 3.042 0 0 0 .027.439a5.185 5.185 0 0 0 1.568 3.312a.998.998 0 0 0-.066.77a5.204 5.204 0 0 0 2.362 2.922a7.465 7.465 0 0 1-3.59.448A1 1 0 0 0 1.45 19.3a12.942 12.942 0 0 0 7.01 2.061a12.788 12.788 0 0 0 12.465-9.363a12.822 12.822 0 0 0 .535-3.646l-.001-.2a5.77 5.77 0 0 0 1.532-4.202Zm-3.306 3.212a.995.995 0 0 0-.234.702c.01.165.009.331.009.488a10.824 10.824 0 0 1-.454 3.08a10.685 10.685 0 0 1-10.546 7.93a10.938 10.938 0 0 1-2.55-.301a9.48 9.48 0 0 0 2.942-1.564a1 1 0 0 0-.602-1.786a3.208 3.208 0 0 1-2.214-.935q.224-.042.445-.105a1 1 0 0 0-.08-1.943a3.198 3.198 0 0 1-2.25-1.726a5.3 5.3 0 0 0 .545.046a1.02 1.02 0 0 0 .984-.696a1 1 0 0 0-.4-1.137a3.196 3.196 0 0 1-1.425-2.673c0-.066.002-.133.006-.198a13.014 13.014 0 0 0 8.21 3.48a1.02 1.02 0 0 0 .817-.36a1 1 0 0 0 .206-.867a3.157 3.157 0 0 1-.087-.729a3.23 3.23 0 0 1 3.226-3.226a3.184 3.184 0 0 1 2.345 1.02a.993.993 0 0 0 .921.298a9.27 9.27 0 0 0 1.212-.322a6.681 6.681 0 0 1-1.026 1.524Z" />
+              </svg>
+            </a>
+          </div>
+
+          <div class="cardContent">
+            <div class="cardBg"></div>
+            <a href="https://discord.gg/aTBCw8hg">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <g fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                  <path d="M5.5 16c5 2.5 8 2.5 13 0" />
+                  <path d="m15.5 17.5l1 2s4.171-1.328 5.5-3.5c0-1 .53-8.147-3-10.5c-1.5-1-4-1.5-4-1.5l-1 2h-2" />
+                  <path d="m8.528 17.5l-1 2s-4.171-1.328-5.5-3.5c0-1-.53-8.147 3-10.5c1.5-1 4-1.5 4-1.5l1 2h2" />
+                  <path
+                    d="M8.5 14c-.828 0-1.5-.895-1.5-2s.672-2 1.5-2s1.5.895 1.5 2s-.672 2-1.5 2Zm7 0c-.828 0-1.5-.895-1.5-2s.672-2 1.5-2s1.5.895 1.5 2s-.672 2-1.5 2Z" />
+                </g>
+              </svg>
+            </a>
+          </div>
+
+          <div class="cardContent">
+            <div class="cardBg"></div>
+            <a href="https://medium.com/@zodicon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+                <circle cx="13.979" cy="24" r="10.479" fill="none" stroke="black" stroke-linecap="round"
+                  stroke-width="3" stroke-linejoin="round" />
+                <ellipse cx="32.318" cy="24" fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round"
+                  stroke-width="3" rx="4.934" ry="10.479" />
+                <ellipse cx="42.339" cy="24" fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round"
+                  stroke-width="3" rx="2.161" ry="10.479" />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div> --}}
+
+</div>
